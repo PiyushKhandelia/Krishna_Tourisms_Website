6a9ff6f6505ea21217e8e32c2ee032f473b0d26c
piyushkhandelia <piyushkhandelia2002@gmail.com>
2022-12-31 22:22:22 +0530

    Push New Code for home

--- js/list.js
+++ js/list.js
@@ -1,4 +1,4 @@
-let menuToggle = document.querySelector('.toogle');
+let menuToggle = document.querySelector('.toggle');
 let navigation = document.querySelector('.navigation')
 menuToggle.onclick = function() {
     menuToggle.classList.toggle('active');
@@ -16,24 +16,3 @@ for (let i = 0; i<list.length; i++) {
         list[i].className = 'list active';
     } 
 }
-
-/*
-let menuToggle = document.querySelector('.toogle');
-let navigation = document.querySelector('.navigation');
-menuToggle.onclick = function() {
-    menuToggle.classList.toggle('active');
-    navigation.classList.toggle('active');
-}
-
-//add active class in selected list item
-let list = document.querySelectorAll('.list');
-for (let i = 0; i<list.length(); i++) {
-    list[i].onclick = function(){
-        let j = 0;
-        while(j < list.length()){
-            list[j++].className = 'list';
-        }
-        list[i].className = 'list active';
-    } 
-}
-*/
