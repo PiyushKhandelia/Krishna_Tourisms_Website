db90787a4d1a430f86ff1fed63ffb9d45fa28704
piyushkhandelia <piyushkhandelia2002@gmail.com>
2022-12-31 10:26:45 +0530

    Footer Changes Edit

--- js/list.js
+++ js/list.js
@@ -7,10 +7,10 @@ menuToggle.onclick = function() {
 
 //add active class in selected list item
 let list = document.querySelectorAll('.list');
-for (let i = 0; i<list.length; i++) {
+for (let i = 0; i<list.length(); i++) {
     list[i].onclick = function(){
         let j = 0;
-        while(j < list.length){
+        while(j < list.length()){
             list[j++].className = 'list';
         }
         list[i].className = 'list active';
